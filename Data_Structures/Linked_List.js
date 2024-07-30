e004dcb6b475c4e32074e9fb24f9d1448c5537e4
Mishal <mishal9990@gmail.com>
2024-07-30 16:56:57 +0200

    linked list ds added

--- Data_Structures/Linked_List.js
+++ Data_Structures/Linked_List.js
@@ -1,40 +1,61 @@
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null
-        this.size = 0
-    }
-
-  isEmpty() {
-    return this.size === 0
+class linkedList {
+  constructor(){
+    this.head = null
+    this.size = null
   }
 
-  getSize() {
-     return this.size
+  isEmpty(){
+    return this.size === 0
+  }
+  
+  getSize(){
+    return this.size
   }
 
   prepend(value) {
     const node = new Node(value)
     if(this.isEmpty()) {
-      this.head = node
-    } else{
+      this.head = node;
+    } else {
       node.next = this.head
       this.head = node
     }
     this.size++
   }
+
+  print() {
+    if(this.isEmpty()) {
+      console.log('list is empty')
+    } else {
+      let curr = this.head;
+      let listValues = '';
+
+      while(curr) {
+        listValues += `${curr.value}-->`
+        curr = curr.next
+      }
+      console.log(listValues)
+    }
+  }
 }
 
-const list = new LinkedList()
-console.log('list is empty ? ', list.isEmpty())
-console.log('List size', list.getSize())
+const list = new linkedList()
+console.log('list is empty ?', list.isEmpty())
+console.log('list is empty ?', list.getSize())
+
+list.print()
 
 list.prepend(10)
+list.print()
+console.log('list is empty ?', list.getSize())
+
 list.prepend(20)
 list.prepend(30)
+list.print()
